6862fbd8d62f7db5b9bc178a06c16e57ac36a87e
Sandoche Adittane <sandoche@protonmail.com>
2022-07-21 12:01:50 +0100

    Work in progress

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const { utils } = require("near-api-js");
+const { convertPublicKey, convertSecretKey } = require("ed2curve");
+
 console.log("[near-js-encryption-box] Starting proof of concept");
+
+const keyPairSender = utils.key_pair.KeyPairEd25519.fromRandom();
+const keyPairReceiver = utils.key_pair.KeyPairEd25519.fromRandom();
+
+const convertedPublicKeyReceiver = convertPublicKey(
+  keyPairReceiver.getPublicKey().data
+);
+
+console.log(keyPairSender);
