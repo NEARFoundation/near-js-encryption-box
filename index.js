ad4fd86755874af51df53a781277ac7626b59758
Sandoche Adittane <sandoche@protonmail.com>
2022-07-21 12:41:49 +0100

    Encoding works

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const { utils } = require("near-api-js");
 const { convertPublicKey, convertSecretKey } = require("ed2curve");
 const { encodeBase64, decodeBase64, decodeUTF8 } = require("tweetnacl-util");
 const { baseDecode } = require("borsh");
+const { box } = require("tweetnacl");
+const randomBytes = require("random-bytes");
 
-console.log("[near-js-encryption-box] Starting proof of concept");
+console.log("[near-js-encryption-box] Encoding secret message");
 
 const keyPairSender = utils.key_pair.KeyPairEd25519.fromRandom();
 const keyPairReceiver = utils.key_pair.KeyPairEd25519.fromRandom();
@@ -17,5 +19,20 @@ const privateAndPublicKeySender = decodeBase64(
 );
 
 const privateKeySender = privateAndPublicKeySender.slice(0, 32);
-
 const convertedPrivateKeySender = convertSecretKey(privateKeySender);
+
+const message = "Hello world";
+
+const messageEncoded = decodeUTF8(message);
+const nonceEncoded = randomBytes.sync(24);
+
+const secret = box(
+  messageEncoded,
+  nonceEncoded,
+  convertedPublicKeyReceiver,
+  convertedPrivateKeySender
+);
+const secretString = encodeBase64(secret);
+
+console.log("[near-js-encryption-box] Secret:", secretString);
+console.log("[near-js-encryption-box] Nonce:", nonceEncoded.toString("base64"));
