8f4568c6a8344a694294c2a9503f16ee6604a945
Sandoche Adittane <sandoche@protonmail.com>
2022-07-21 14:38:49 +0100

    Updating the test

--- test/near-js-encryption-box.test.ts
+++ test/near-js-encryption-box.test.ts
@@ -9,19 +9,19 @@ describe('Creating a secret box with Alice and Bob key pairs and verify the secr
   it('Should the message open should be the same as the message sent', () => {
     const messageSent = 'Hello world';
 
-    const publicKeyAlice = encodeBase64(keyPairAlice.getPublicKey().data);
-    const privateKeyBob = keyPairBob.secretKey;
-
-    const { secret, nonce } = create(
-      messageSent,
-      publicKeyAlice,
-      privateKeyBob
-    );
-
     const publicKeyBob = encodeBase64(keyPairBob.getPublicKey().data);
     const privateKeyAlice = keyPairAlice.secretKey;
 
-    const messageReceived = open(secret, publicKeyBob, privateKeyAlice, nonce);
+    const { secret, nonce } = create(
+      messageSent,
+      publicKeyBob,
+      privateKeyAlice
+    );
+
+    const publicKeyAlice = encodeBase64(keyPairAlice.getPublicKey().data);
+    const privateKeyBob = keyPairBob.secretKey;
+
+    const messageReceived = open(secret, publicKeyAlice, privateKeyBob, nonce);
 
     expect(messageSent).toBe(messageReceived);
   });
