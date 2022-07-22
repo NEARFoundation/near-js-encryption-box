a5ba9e2549e4b6825867c203869e4294b4d0a863
Sandoche Adittane <sandoche@protonmail.com>
2022-07-22 09:31:59 +0100

    Adding test cases

--- test/near-js-encryption-box.test.ts
+++ test/near-js-encryption-box.test.ts
@@ -4,25 +4,36 @@ import { utils } from 'near-api-js';
 describe('Creating a secret box with Alice and Bob key pairs randomly generated from NEAR JS LIBRARY', () => {
   const keyPairAlice = utils.key_pair.KeyPairEd25519.fromRandom();
   const keyPairBob = utils.key_pair.KeyPairEd25519.fromRandom();
+  const keyPairCharlie = utils.key_pair.KeyPairEd25519.fromRandom();
 
-  it('Should the message open should be the same as the message sent', () => {
-    const messageSent = 'Hello world';
+  const messageSent = 'Hello world';
 
-    const publicKeyBob = keyPairBob.getPublicKey().toString();
-    const privateKeyAlice = keyPairAlice.secretKey;
+  const publicKeyBob = keyPairBob.getPublicKey().toString();
+  const privateKeyAlice = keyPairAlice.secretKey;
 
-    const { secret, nonce } = create(
-      messageSent,
-      publicKeyBob,
-      privateKeyAlice
-    );
+  const { secret, nonce } = create(messageSent, publicKeyBob, privateKeyAlice);
 
+  it('Should contain the same message sent by Alice opened by Bob', () => {
     const publicKeyAlice = keyPairAlice.getPublicKey().toString();
     const privateKeyBob = keyPairBob.secretKey;
 
     const messageReceived = open(secret, publicKeyAlice, privateKeyBob, nonce);
 
     expect(messageSent).toBe(messageReceived);
+  });
+
+  it('Should not let Charlie open the box created by Alice and sent to Bob', () => {
+    const publicKeyAlice = keyPairAlice.getPublicKey().toString();
+    const privateKeyCharlie = keyPairCharlie.secretKey;
+
+    const messageReceived = open(
+      secret,
+      publicKeyAlice,
+      privateKeyCharlie,
+      nonce
+    );
+
+    expect(messageReceived).toBe(null);
   });
 });
 
@@ -39,7 +50,7 @@ describe('Creating a secret box with Alice and Bob key pairs generated with NEAR
       '09430fbb4310e17cec8c8639d6b2c6c59c28ca5c9fdd9680834737aa1aeadfec',
   };
 
-  it('Should the message open should be the same as the message sent', () => {
+  it('Should contain the same message sent by Alice opened by Bob', () => {
     const messageSent = 'Hello world';
 
     const publicKeyBob = keyPairBob.publicKey;
@@ -73,7 +84,7 @@ describe('Creating a secret box with Alice and Bob key pairs having ed25519: pre
       'ed25519:09430fbb4310e17cec8c8639d6b2c6c59c28ca5c9fdd9680834737aa1aeadfec',
   };
 
-  it('Should the message open should be the same as the message sent', () => {
+  it('Should contain the same message sent by Alice opened by Bob', () => {
     const messageSent = 'Hello world';
 
     const publicKeyBob = keyPairBob.publicKey;
@@ -107,7 +118,7 @@ describe('Creating a secret box with Alice and Bob key pairs without ed25519: pr
       '09430fbb4310e17cec8c8639d6b2c6c59c28ca5c9fdd9680834737aa1aeadfec',
   };
 
-  it('Should the message open should be the same as the message sent', () => {
+  it('Should contain the same message sent by Alice opened by Bob', () => {
     const messageSent = 'Hello world';
 
     const publicKeyBob = keyPairBob.publicKey;
@@ -130,19 +141,19 @@ describe('Creating a secret box with Alice and Bob key pairs without ed25519: pr
 
 describe('Creating a secret box for Alice only', () => {
   const keyPairAlice = utils.key_pair.KeyPairEd25519.fromRandom();
+  const keyPairBob = utils.key_pair.KeyPairEd25519.fromRandom();
 
-  it('Should the message open should be the same as the message sent', () => {
-    const messageSent = 'Hello world';
+  const messageSent = 'Hello world';
+  const privateKeyAlice = keyPairAlice.secretKey;
+  const publicKeyAlice = keyPairAlice.getPublicKey().toString();
 
-    const privateKeyAlice = keyPairAlice.secretKey;
-    const publicKeyAlice = keyPairAlice.getPublicKey().toString();
+  const { secret, nonce } = create(
+    messageSent,
+    publicKeyAlice,
+    privateKeyAlice
+  );
 
-    const { secret, nonce } = create(
-      messageSent,
-      publicKeyAlice,
-      privateKeyAlice
-    );
-
+  it('Should contain the same message', () => {
     const messageReceived = open(
       secret,
       publicKeyAlice,
@@ -151,5 +162,13 @@ describe('Creating a secret box for Alice only', () => {
     );
 
     expect(messageSent).toBe(messageReceived);
+  });
+
+  it('Should not let Bob open the box created by Alice for herself', () => {
+    const privateKeyBob = keyPairBob.secretKey;
+
+    const messageReceived = open(secret, publicKeyAlice, privateKeyBob, nonce);
+
+    expect(messageReceived).toBe(null);
   });
 });
